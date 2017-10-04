658a2f33b809daa619dc5def8cc365c2cabb2e7f
GreenXenith <bl00ntdmonkey@yahoo.com>
2017-10-03 17:05:01 -0700

    Added bagel, muffin, and cake.

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ minetest.register_craftitem("pumpkinspice:pumpkinspice", {
 minetest.register_craft({
 	type = "shapeless",
 	output = "pumpkinspice:pumpkinspice 2",
-	recipe = {"farming:pumpkin_slice"}
+	recipe = {"farming:pumpkin_slice"},
 })
 
 -- Latte
@@ -65,4 +65,106 @@ minetest.register_craft({
 	type = "shapeless",
 	output = "pumpkinspice:pumpkinspice_bread",
 	recipe = {"farming:bread", "pumpkinspice:pumpkinspice"}
+})
+
+-- Muffins
+
+-- base
+minetest.register_craftitem("pumpkinspice:muffin", {
+	description = "Muffin",
+	inventory_image = "muffin_base.png",
+	on_use = minetest.item_eat(2),
+})
+
+minetest.register_craft({
+	output = "pumpkinspice:muffin 2",
+	recipe = {
+		{"default:paper", "farming:bread", "default:paper"},
+		{"default:paper", "farming:bread", "default:paper"},
+	},
+})
+
+-- re-register blueberry
+minetest.register_craftitem(":farming:muffin_blueberry", {
+	description = "Blueberry Muffin",
+	inventory_image = "farming_blueberry_muffin.png",
+	on_use = minetest.item_eat(3),
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:muffin_blueberry 2",
+	recipe = {"farming:blueberries", "pumpkinspice:muffin"},
+})
+
+-- pumpkinspice muffin
+minetest.register_craftitem("pumpkinspice:muffin_pumpkinspice", {
+	description = "Pumpkin Spice Muffin",
+	inventory_image = "muffin_pumpkinspice.png",
+	on_use = minetest.item_eat(4),
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "pumpkinspice:muffin_pumpkinspice 2",
+	recipe = {"pumpkinspice:pumpkinspice", "pumpkinspice:muffin"},
+})
+
+-- Bagels
+
+-- base
+minetest.register_craftitem("pumpkinspice:bagel", {
+	description = "Bagel",
+	inventory_image = "bagel.png",
+	on_use = minetest.item_eat(2),
+})
+
+minetest.register_craft({
+	output = "pumpkinspice:bagel 5",
+	recipe = {
+		{"farming:bread", "farming:bread", "farming:bread"},
+		{"farming:bread", "", "farming:bread"},
+		{"farming:bread", "farming:bread", "farming:bread"},
+	},
+})
+
+-- pumpkinspice bagel
+minetest.register_craftitem("pumpkinspice:bagel_pumpkinspice", {
+	description = "Pumpkin Spice Bagel",
+	inventory_image = "pumpkinspice_bagel.png",
+	on_use = minetest.item_eat(4),
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "pumpkinspice:bagel_pumpkinspice 2",
+	recipe = {"pumpkinspice:pumpkinspice", "pumpkinspice:bagel"},
+})
+
+-- Cake
+
+minetest.register_craftitem("pumpkinspice:pumpkinspice_cakebatter", {
+	description = "Pumpkin Spice Cake Batter",
+	inventory_image = "pumpkinspice_cakebatter.png",
+})
+
+minetest.register_craft({
+	output = "pumpkinspice:pumpkinspice_cakebatter",
+	recipe = {
+		{"pumpkinspice:pumpkinspice", "pumpkinspice:pumpkinspice", "pumpkinspice:pumpkinspice"},
+		{"farming:flour", "farming:sugar", "farming:flour"},
+	},
+})
+
+minetest.register_craftitem("pumpkinspice:pumpkinspice_cake", {
+    description = "Pumpkin Spice Cake",
+    inventory_image = "pumpkinspice_cake.png",
+    on_use = minetest.item_eat(12),
+})
+
+minetest.register_craft({
+    type = "cooking",
+    output = "pumpkinspice:pumpkinspice_cake",
+    recipe = "pumpkinspice:pumpkinspice_cakebatter",
+    cooktime = 25,
 })
